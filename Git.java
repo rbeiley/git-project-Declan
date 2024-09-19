2b4aa6f779a1e6372b4974083bc840dc7525dc03
Declan <dmurphy1@hwemail.com>
2024-09-19 13:03:59 -0700

    Created git initializer (WIP)
    Creates git directory, objects directory and index file (WIP)

--- Git.java
+++ Git.java
@@ -1,6 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 public class Git
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        File git = new File("git");
+        if (!git.isDirectory())
+            git.mkdir();
+        File objects = new File("git/objects");
+        if (!objects.isDirectory())
+            objects.mkdir();
+        File index = new File("git","index");
+        index.createNewFile();
     }
 }
