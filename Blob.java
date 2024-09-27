a09e915afbd28ce3d7ba197d1bb897c0afa6278f
Declan <dmurphy1@hwemail.com>
2024-09-26 21:44:00 -0700

    Added Compression to Blobs
    Global Variable for Blobs called 'compress' which compresses Blob data into zip files.
    Updated Blob Tester Accordingly.

--- Blob.java
+++ Blob.java
@@ -4,15 +4,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class Blob
 {
+    public boolean compress = true;
     public Blob (File file) throws IOException
     {
         String code = getFileData(file);
         String hash = Sha1.encryptThisString(code);
-        File blob = new File("git/objects/" + hash);
-        blob.createNewFile();
-        FileWriter writeToBlob = new FileWriter("git/objects/" + hash);
-        writeToBlob.append(code);
-        writeToBlob.close();
+        if (compress)
+        {
+            File blob = ZipFile.zipFile(file.getName());
+            blob.renameTo(new File("git/objects/" + hash));
+        }
+        else
+        {
+            File blob = new File("git/objects/" + hash);
+            blob.createNewFile();
+            FileWriter writeToBlob = new FileWriter("git/objects/" + hash);
+            writeToBlob.append(code);
+            writeToBlob.close();
+        }
         FileWriter writeToIndex = new FileWriter("git/index", true);
         writeToIndex.append(hash + ' ' + file.getName() + '\n');
         writeToIndex.close();
--- BlobTester.java
+++ BlobTester.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
+
 public class BlobTester {
     public static void main(String[] args) {
         try {
-            //Creates test file if it doesn't exist and converts it into a blob
+            // Creates test file if it doesn't exist and converts it into a blob
             File test = new File("BlobTestFile.txt");
             System.out.println("New Test File Created: " + test.createNewFile());
-            FileWriter write = new FileWriter("BlobTestFile.txt",true);
+            FileWriter write = new FileWriter("BlobTestFile.txt", true);
             String testString = "AHHHHHHHHHHHHH";
             for (int i = 0; i < testString.length(); i++) {
                 write.append(testString.charAt(i));
@@ -18,51 +19,53 @@ public class BlobTester {
             write.close();
             Blob testBlob = new Blob(test);
 
-            //Testing
+            // Testing
             String encrypted = Sha1.encryptThisString(testString);
             boolean pass1 = false;
             boolean pass2 = true;
 
-            //Testing if Index was properly created
-            BufferedReader checkIndex = new BufferedReader(new FileReader ("git/index"));
-            while (checkIndex.ready())
-            {
-                if (checkIndex.readLine().equals(encrypted + " " + test.getName()))
-                {
+            // Testing if Index was properly created
+            BufferedReader checkIndex = new BufferedReader(new FileReader("git/index"));
+            while (checkIndex.ready()) {
+                if (checkIndex.readLine().equals(encrypted + " " + test.getName())) {
                     System.out.println("Blob Input Correctly in Index");
                     pass1 = true;
                 }
             }
             checkIndex.close();
 
-            //Testing If Blob was Properly Created
-            if (new File("git/objects/" + encrypted).exists())
-                System.out.println("Blob object created and named properly : " + encrypted);
-            BufferedReader checkBlob = new BufferedReader(new FileReader ("git/objects/" + encrypted));
-            int counter = 0;
-            while (checkBlob.ready())
-            {
-                if (checkBlob.read() != testString.charAt(counter))
-                {
-                    System.out.println("File Contents are not equal");
-                    pass2 = false;
+            // Testing If Blob was Properly Created
+            if (!testBlob.compress) {
+                if (new File("git/objects/" + encrypted).exists())
+                    System.out.println("Blob object created and named properly : " + encrypted);
+                BufferedReader checkBlob = new BufferedReader(new FileReader("git/objects/" + encrypted));
+                int counter = 0;
+                while (checkBlob.ready()) {
+                    if (checkBlob.read() != testString.charAt(counter)) {
+                        System.out.println("File Contents are not equal");
+                        pass2 = false;
+                    }
+                    counter++;
                 }
-                counter++;
+                checkBlob.close();
+            }
+            else
+            {
+                System.out.println("File Is Compressed");
             }
 
-            //Final Check
+            // Final Check
             if (pass1 && pass2)
                 System.out.println("Successful Blob Test");
             else
                 System.out.println("Unsuccessful Blob Test");
-            checkBlob.close();
         } catch (Exception e) {
             System.out.println("File Not Found");
         }
     }
-    public static void reset ()
-    {
-        File index = new File ("/git/index");
+
+    public static void reset() {
+        File index = new File("/git/index");
         index.delete();
         FileUtils.deleteDirectory("git/objects");
     }
